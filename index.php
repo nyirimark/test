2fdbb9d41f429dcc18ff241aba6bf5951fbb1823
nyirimark <nyiri.mark@gmail.com>
2018-08-06 14:08:35 +0200

    first commit

--- index.php
+++ index.php
@@ -56,7 +56,12 @@
         <h1>Hallo <?php echo 'APPUiO'?></h1>
         <p class="lead">APPUiO Example Source To Image PHP</p>
       </div>
-
+    <?php
+    echo gethostname(); // may output e.g,: sandie
+    
+    // Or, an option that also works before PHP 5.3
+    echo php_uname('n'); // may output e.g,: sandie
+    ?>
     </div><!-- /.container -->
 
 
